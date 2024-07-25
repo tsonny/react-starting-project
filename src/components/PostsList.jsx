efd5a551fbfcf8aba776e55f6a4051c1a1d1b131
Tiffany Sonny <tiffanysonny@gmail.com>
2024-07-24 19:02:30 -0700

    added useEffect hook to get posts from dummy server

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,29 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./PostsList.module.css";
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts')
+      const resData = await response.json();
+      setPosts(resData.posts);
+      }
+      fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts',{
+      method: 'POST', 
+      body: JSON.stringify(postData),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
@@ -27,10 +43,7 @@ function PostsList({ isPosting, onStopPosting }) {
           ))}
         </ul>
       )}
-      {posts.length === 0 && 
-      (
-        <div>You are the first poster.</div>
-      )}
+      {posts.length === 0 && <div>You are the first poster.</div>}
     </>
   );
 }
