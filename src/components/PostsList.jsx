33c51e607e1aab059dbbf4db6954499ec2bb19ef
Tiffany Sonny <tiffanysonny@gmail.com>
2024-07-25 00:40:52 -0700

    added loader

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,21 +1,9 @@
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
 
 function PostsList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setFetching(false);
-    }
-    fetchPosts();
-  }, []);
+  const posts = useLoaderData();
 
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
@@ -30,22 +18,17 @@ function PostsList() {
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div>You are the first poster.</div>
       )}
 
-      {isFetching && (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <p>Loading posts ...</p>
-        </div>
-      )}
     </>
   );
 }
