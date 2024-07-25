bf7f1c0d1c812e6270ad12575a3e661733e82dda
Tiffany Sonny <tiffanysonny@gmail.com>
2024-07-25 01:17:12 -0700

    Updated with actions for submit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Posts, { loader as postsLoader } from "./routes/Posts";
-import NewPost from "./routes/NewPost";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayouts";
 import "./index.css";
 
@@ -19,6 +19,7 @@ const router = createBrowserRouter([
           {
             path: "/create-post",
             element: <NewPost />,
+            action: newPostAction,
           },
         ],
       },
